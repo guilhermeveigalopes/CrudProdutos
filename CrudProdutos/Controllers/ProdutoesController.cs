9fadd06018e4b04b50d354df7fba417f4def274d
guilhermeveigalopes <guilherme.vwiga@gmail.com>
2024-02-26 17:06:42 -0300

    Adicionar arquivos de projeto.

---
+++ CrudProdutos/Controllers/ProdutoesController.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using CrudProdutos.Models;
+
+namespace CrudProdutos.Controllers
+{
+    public class ProdutoesController : Controller
+    {
+        private readonly DbLojaSenaiContext _context;
+
+        public ProdutoesController(DbLojaSenaiContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Produtoes
+        public async Task<IActionResult> Index()
+        {
+            var dbLojaSenaiContext = _context.Produtos.Include(p => p.IdCategoriaNavigation).Include(p => p.IdFornecedorNavigation);
+            return View(await dbLojaSenaiContext.ToListAsync());
+        }
+
+        // GET: Produtoes/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Produtos == null)
+            {
+                return NotFound();
+            }
+
+            var produto = await _context.Produtos
+                .Include(p => p.IdCategoriaNavigation)
+                .Include(p => p.IdFornecedorNavigation)
+                .FirstOrDefaultAsync(m => m.Idproduto == id);
+            if (produto == null)
+            {
+                return NotFound();
+            }
+
+            return View(produto);
+        }
+
+        // GET: Produtoes/Create
+        public IActionResult Create()
+        {
+            ViewData["IdCategoria"] = new SelectList(_context.Categoria, "IdCategoria", "IdCategoria");
+            ViewData["IdFornecedor"] = new SelectList(_context.Fornecedors, "IdFornecedor", "IdFornecedor");
+            return View();
+        }
+
+        // POST: Produtoes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Idproduto,Descricao,IdCategoria,IdFornecedor")] Produto produto)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(produto);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["IdCategoria"] = new SelectList(_context.Categoria, "IdCategoria", "IdCategoria", produto.IdCategoria);
+            ViewData["IdFornecedor"] = new SelectList(_context.Fornecedors, "IdFornecedor", "IdFornecedor", produto.IdFornecedor);
+            return View(produto);
+        }
+
+        // GET: Produtoes/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Produtos == null)
+            {
+                return NotFound();
+            }
+
+            var produto = await _context.Produtos.FindAsync(id);
+            if (produto == null)
+            {
+                return NotFound();
+            }
+            ViewData["IdCategoria"] = new SelectList(_context.Categoria, "IdCategoria", "IdCategoria", produto.IdCategoria);
+            ViewData["IdFornecedor"] = new SelectList(_context.Fornecedors, "IdFornecedor", "IdFornecedor", produto.IdFornecedor);
+            return View(produto);
+        }
+
+        // POST: Produtoes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Idproduto,Descricao,IdCategoria,IdFornecedor")] Produto produto)
+        {
+            if (id != produto.Idproduto)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(produto);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ProdutoExists(produto.Idproduto))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["IdCategoria"] = new SelectList(_context.Categoria, "IdCategoria", "IdCategoria", produto.IdCategoria);
+            ViewData["IdFornecedor"] = new SelectList(_context.Fornecedors, "IdFornecedor", "IdFornecedor", produto.IdFornecedor);
+            return View(produto);
+        }
+
+        // GET: Produtoes/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Produtos == null)
+            {
+                return NotFound();
+            }
+
+            var produto = await _context.Produtos
+                .Include(p => p.IdCategoriaNavigation)
+                .Include(p => p.IdFornecedorNavigation)
+                .FirstOrDefaultAsync(m => m.Idproduto == id);
+            if (produto == null)
+            {
+                return NotFound();
+            }
+
+            return View(produto);
+        }
+
+        // POST: Produtoes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Produtos == null)
+            {
+                return Problem("Entity set 'DbLojaSenaiContext.Produtos'  is null.");
+            }
+            var produto = await _context.Produtos.FindAsync(id);
+            if (produto != null)
+            {
+                _context.Produtos.Remove(produto);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ProdutoExists(int id)
+        {
+          return (_context.Produtos?.Any(e => e.Idproduto == id)).GetValueOrDefault();
+        }
+    }
+}
